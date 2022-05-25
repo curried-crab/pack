e63c3380ff15b2554d9a3a31c1284e366acaf5bb
newrankyangjingyu <yjy15680489038@163.com>
2022-05-25 17:44:55 +0800

    test: test module

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import * as test from "./test.ts";
+// import * as test from "./test.ts";
 
-// import { isArray } from "lodash-es";
+import { isArray } from "lodash-es";
 
-// const arr = [];
-// console.log("哈哈哈哈是", isArray(arr));
+const arr = [];
+console.log("哈哈哈哈是", isArray(arr));
 
-function a1() {}
-test.f();
+// function a1() {}
+// test.f();
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,20 +1,33 @@
-const path = require("path")
+import * as path from "path";
 
-module.exports = {
+import _ from "lodash"
+import { fileURLToPath } from "url"
+
+const test = require("./test")
+
+
+console.log("--->", _.isArray([]))
+test.test()
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
   entry: path.resolve(__dirname, "../src/index.ts"),
   module: {
     rules: [
       {
         test: /\.(ts|tsx|js|jsx)?$/,
-        loader: 'esbuild-loader',
+        loader: "esbuild-loader",
         exclude: /node_modules/,
         options: {
-          target: 'es2015'  // Syntax to compile to (see options below for possible values)
-        }
+          target: "es2015", // Syntax to compile to (see options below for possible values)
+        },
       },
-    ]
+    ],
   },
   optimization: {
-    usedExports: true
-  }
-}
+    usedExports: true,
+  },
+};
