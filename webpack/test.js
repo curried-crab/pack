a93c9966ab1cc070256233ed4f36f396efa49567
newrankyangjingyu <yjy15680489038@163.com>
2022-05-26 13:33:57 +0800

    feat: add require.js

--- webpack/test.js
+++ webpack/test.js
@@ -1,7 +1,16 @@
-function test() {
-  console.log(".....test.....")
-}
-
-exports.module = {
-  test
-}
+(function (root, factory) {
+  if (typeof define === 'function' && (define.amd || define.cmd)) {
+    //AMD,CMD
+    define(['b'], function (b) {
+      return (root.returnExportsGlobal = factory(b))
+    });
+  } else if (typeof module === 'object' && module.exports) {
+    //Node, CommonJS之类的
+    module.exports = factory(require('b'));
+  } else {
+    //公开暴露给全局对象
+    root.returnExports = factory(root.b);
+  }
+}(this, function (b) {
+  return {};
+}));
